3f285c5ef68940974f074636169dfa7715f42252
zaid <amonsefra13@gmail.com>
2025-06-19 23:55:27 +0700

    Refactor layout and navigation structure; add settings screen and update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
-import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+        tabBarIcon: ({ color, size }) => {
+          let iconName: string;
+
+          if (route.name === 'index') {
+            iconName = 'home';
+          } else if (route.name === 'settings') {
+            iconName = 'settings';
+          } else {
+            iconName = 'ellipse';
+          }
+
+          return <Ionicons name={iconName as any} size={size} color={color} />;
+        }
+      })}
+    />
+  );
 }
--- app/screens/setting.tsx
+++ app/screens/setting.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
-export default function HomeScreen() {
+export default function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>This is the Home Screen</Text>
+      <Text>This is the Settings Screen</Text>
     </View>
   );
 }
